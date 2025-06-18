4f90020ea22d2e21fd87722e82764a7bc8e750d1
Ege Darici <ege@vespertool.com>
2025-06-18 17:32:53 +0200

    Add footer component

---
+++ components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <div
+      class="flex font-mono justify-between p-4 max-md:items-center fixed bottom-0 left-0 w-full z-50 text-white"
+    >
+      <div>
+        <p class="text-sm">© 2025 Whelm Int.</p>
+      </div>
+      <div class="max-md:hidden flex gap-2 max-md:items-center">
+        <NuxtLink to="/">
+          <font-awesome icon="fa-brands fa-instagram" class="" />
+        </NuxtLink>
+        <NuxtLink to="/">
+          <font-awesome icon="fa-brands fa-twitter" class="" />
+        </NuxtLink>
+      </div>
+    </div>
+  </footer>
+</template>
